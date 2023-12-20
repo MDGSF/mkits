993f3732471df5942512af60641637c89381c29a
HuangJian <1342042894@qq.com>
2023-12-20 10:38:44 +0800

    [mckits] update mckits.h

--- mckits/mckits.h
+++ mckits/mckits.h
@@ -1,4 +1,14 @@
 #ifndef MKITS_MCKITS_MCKITS_H_
 #define MKITS_MCKITS_MCKITS_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "mckits_encoding_binary.h"
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
