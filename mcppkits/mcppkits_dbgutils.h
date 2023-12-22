106dffc7a2c5defc37c3f5fe4b866181062413bf
HuangJian <1342042894@qq.com>
2023-12-22 15:34:19 +0800

    [mcppkits] add dbgutils, update fps

---
+++ mcppkits/mcppkits_dbgutils.h
@@ -0,0 +1,52 @@
+#ifndef MKITS_MCPPKITS_MCPPKITS_DBGUTILS_H_
+#define MKITS_MCPPKITS_MCPPKITS_DBGUTILS_H_
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+namespace mcppkits {
+
+template <typename T>
+void PrintVec01(const std::vector<T> vec, const std::string& name) {
+  int i = 0;
+  std::cerr << "vector: " << name << ", size: " << vec.size() << std::endl;
+  for (const T& entry : vec) {
+    std::cerr << "vec[" << i++ << "]: " << entry << std::endl;
+  }
+}
+
+template <typename T>
+void PrintVec02(const std::vector<T> vec, const std::string& name) {
+  int i = 0;
+  std::cout << "vector: " << name << ", size: " << vec.size() << std::endl;
+  std::cout << "[";
+  for (auto v : vec) {
+    if (i > 0) {
+      std::cout << ", ";
+    }
+    std::cout << v;
+    ++i;
+  }
+  std::cout << "]" << std::endl;
+}
+
+template <typename KeyType, typename ValType>
+void PrintMap(const std::map<KeyType, ValType>& m) {
+  int i = 0;
+  std::cout << "{";
+  for (auto kv : m) {
+    if (i > 0) {
+      std::cout << ", ";
+    }
+
+    std::cout << kv.first << ": " << kv.second;
+    ++i;
+  }
+  std::cout << "}" << std::endl;
+}
+
+}  // namespace mcppkits
+
+#endif
--- mcppkits/mcppkits_fps.h
+++ mcppkits/mcppkits_fps.h
@@ -2,6 +2,8 @@
 #define MKITS_MCPPKITS_MCPPKITS_FPS_H_
 
 #include <chrono>
+#include <cstdio>
+#include <iostream>
 #include <string>
 
 namespace mcppkits {
@@ -9,14 +11,32 @@ namespace fps {
 
 class CFPS {
  public:
-  CFPS();
-  explicit CFPS(const std::string& name);
+  CFPS() { pre_time_ = std::chrono::steady_clock::now(); }
+
+  explicit CFPS(const std::string &name) : name_(name) {
+    pre_time_ = std::chrono::steady_clock::now();
+  }
+
   virtual ~CFPS() = default;
 
   /*
   @brief: Call triger() function in loop. It will print fps.
   */
-  void triger();
+  void triger() {
+    ++count_;
+
+    auto cur_time = std::chrono::steady_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+                       cur_time - pre_time_)
+                       .count();
+    if (elapsed > 500) {
+      fps_ = (count_ - pre_count_) * 1e3 / elapsed;
+      pre_time_ = cur_time;
+      pre_count_ = count_;
+    }
+
+    fprintf(stderr, "[%s] fps = %.2lf", name_.c_str(), fps_);
+  }
 
  private:
   std::string name_ = "";
