4f9ff1305ac3701e3275f98f5f401d6dbcd9d690
HuangJian <1342042894@qq.com>
2023-12-26 19:46:05 +0800

    [mckits] add strconv

---
+++ test/mckits/unittest_strconv.c
@@ -0,0 +1,41 @@
+#include <assert.h>
+#include <string.h>
+
+#include "mckits_strconv.h"
+
+void test01() {
+  const char* str01 = "123";
+  int num = mckits_string_to_int(str01);
+
+  char str02[16] = {0};
+  size_t str02_size = sizeof(str02);
+  mckits_int_to_string(num, str02, str02_size);
+  assert(strcmp(str02, str01) == 0);
+}
+
+void test02() {
+  const char* str01 = "-123";
+  int num = mckits_string_to_int(str01);
+
+  char str02[16] = {0};
+  size_t str02_size = sizeof(str02);
+  mckits_int_to_string(num, str02, str02_size);
+  assert(strcmp(str02, str01) == 0);
+}
+
+void test03() {
+  const char* str01 = "-123";
+  int64_t num = mckits_string_to_int64(str01);
+
+  char str02[16] = {0};
+  size_t str02_size = sizeof(str02);
+  mckits_int64_to_string(num, str02, str02_size);
+  assert(strcmp(str02, str01) == 0);
+}
+
+int main() {
+  test01();
+  test02();
+  test03();
+  return 0;
+}
