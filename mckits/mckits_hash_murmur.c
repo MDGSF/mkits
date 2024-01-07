6a696ce68d7034f8881fbfe5a4f53bff121617ab
HuangJian <1342042894@qq.com>
2024-01-07 11:31:16 +0800

    [mckits] add murmur3 hash function

--- mckits/mckits_hash_murmur.c
+++ mckits/mckits_hash_murmur.c
@@ -1,5 +1,53 @@
 #include "mckits_hash_murmur.h"
 
+uint32_t mckits_murmur3_32(const uint8_t* data, size_t len, uint32_t seed) {
+  uint32_t k = 0;
+  uint32_t h = seed;
+
+  // four byte chunk
+  while (len >= 4) {
+    k = data[0];
+    k |= data[1] << 8;
+    k |= data[2] << 16;
+    k |= data[3] << 24;
+
+    k *= 0xcc9e2d51;
+    k = (k << 15) | (k >> 17);
+    k *= 0x1b873593;
+
+    h ^= k;
+    h = (h << 13) | (h >> 19);
+    h = h * 5 + 0xe6546b64;
+  }
+
+  // remaining bytes
+  k = 0;
+  switch (len) {
+    case 3:
+      k ^= data[2] << 16;
+      /* fall through */
+    case 2:
+      k ^= data[1] << 8;
+      /* fall through */
+    case 1:
+      k ^= data[0];
+  }
+  k *= 0xcc9e2d51;
+  k = (k << 15) | (k >> 17);
+  k *= 0x1b873593;
+  h ^= k;
+
+  // Finalize.
+  h ^= len;
+  h ^= h >> 16;
+  h *= 0x85ebca6b;
+  h ^= h >> 13;
+  h *= 0xc2b2ae35;
+  h ^= h >> 16;
+
+  return h;
+}
+
 uint32_t mckits_murmur2_32(const uint8_t* data, size_t len) {
   uint32_t k = 0;
   uint32_t h = 0 ^ len;
--- mckits/mckits_hash_murmur.h
+++ mckits/mckits_hash_murmur.h
@@ -1,5 +1,6 @@
 /*
-murmur hash function.
+Murmur hash function. MurmurHash is a non-cryptographic hash function suitable
+for general hash-based lookup.
 wiki: https://en.wikipedia.org/wiki/MurmurHash
 */
 #ifndef MKITS_MCKITS_MCKITS_HASH_MURMUR_H_
@@ -11,6 +12,15 @@ wiki: https://en.wikipedia.org/wiki/MurmurHash
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/*
+@brief: Calculate hash code use murmur3 hash function.
+@param data[in]: Input data.
+@param len[in]: Input data size in bytes.
+@param seed[in]: hash code initialize value.
+@return hash code
+*/
+uint32_t mckits_murmur3_32(const uint8_t* data, size_t len, uint32_t seed);
 
 /*
 @brief: Calculate hash code use murmur2 hash function.
