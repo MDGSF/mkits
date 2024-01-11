69d8d1ff414089f4910ed0c195844bedd37a78c0
HuangJian <1342042894@qq.com>
2024-01-11 15:27:38 +0800

    [mckits] add hex encode

--- mckits/mckits_encoding_hex.c
+++ mckits/mckits_encoding_hex.c
@@ -1,1 +1,14 @@
 #include "mckits_encoding_hex.h"
+
+static const char* hextable = "0123456789ABCDEF";
+
+void mckits_hex_encode(uint8_t* dst, size_t dst_size, const uint8_t* src,
+                       size_t src_size) {
+  size_t j = 0;
+  for (size_t i = 0; i < src_size; ++i) {
+    uint8_t v = src[i];
+    dst[j] = hextable[(v >> 4) & 0x0F];
+    dst[j + 1] = hextable[v & 0x0F];
+    j += 2;
+  }
+}
--- mckits/mckits_encoding_hex.h
+++ mckits/mckits_encoding_hex.h
@@ -8,6 +8,16 @@
 extern "C" {
 #endif
 
+/*
+@brief: Hex encode src into dst. dst_size must be greater than src_size * 2.
+@param dst[out]: Destination memory buffer used to store encoded hex binary.
+@param dst_size[in]: Destination memory buffer size.
+@param src[in]: Source memory buffer.
+@param src_size[in]: Source memory buffer size.
+*/
+void mckits_hex_encode(uint8_t* dst, size_t dst_size, const uint8_t* src,
+                       size_t src_size);
+
 #ifdef __cplusplus
 }
 #endif
