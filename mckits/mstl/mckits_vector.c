a1f2bb5c285a33d68a1888670df2b1bf2f458f53
HuangJian <1342042894@qq.com>
2024-01-20 00:40:15 +0800

    [mckits] update vector

--- mckits/mstl/mckits_vector.c
+++ mckits/mstl/mckits_vector.c
@@ -1,5 +1,6 @@
 #include "mckits_vector.h"
 
+#include <assert.h>
 #include <string.h>
 
 #include "mckits_array.h"
@@ -43,7 +44,7 @@ int mckits_vec_is_empty(const struct MckitsVector* mvec) {
   return mvec->size == 0 ? 1 : 0;
 }
 
-static void mckits_vec_growsize(struct MckitsVector* mvec) {
+void mckits_vec_push(struct MckitsVector* mvec, void* value) {
   if (mvec->capacity == 0) {
     // vector is empty
 
@@ -66,23 +67,17 @@ static void mckits_vec_growsize(struct MckitsVector* mvec) {
     mvec->buffer = new_buffer;
     mvec->capacity = new_capacity;
   }
-}
-
-void mckits_vec_push(struct MckitsVector* mvec, void* value) {
-  mckits_vec_growsize(mvec);
 
   void* element = (uint8_t*)mvec->buffer + mvec->size * mvec->element_bytes;
   memcpy(element, value, mvec->element_bytes);
   mvec->size += 1;
 }
 
-void* mckits_vec_pop(struct MckitsVector* mvec) {
-  if (mckits_vec_is_empty(mvec)) {
-    return NULL;
+void mckits_vec_pop(struct MckitsVector* mvec) {
+  if (mvec->size == 0) {
+    return;
   }
-  void* element = (uint8_t*)mvec->buffer + mvec->size * mvec->element_bytes;
   mvec->size -= 1;
-  return element;
 }
 
 void* mckits_vec_get(struct MckitsVector* mvec, size_t index) {
@@ -90,4 +85,61 @@ void* mckits_vec_get(struct MckitsVector* mvec, size_t index) {
     return NULL;
   }
   return (uint8_t*)mvec->buffer + index * mvec->element_bytes;
+}
+
+void mckits_vec_append(struct MckitsVector* mvec, struct MckitsVector* other) {
+  if (mvec->capacity == 0) {
+    // vector is empty
+
+    mvec->buffer = other->buffer;
+    mvec->size = other->size;
+    mvec->capacity = other->capacity;
+    mvec->element_bytes = other->element_bytes;
+
+    other->buffer = 0;
+    other->size = 0;
+    other->capacity = 0;
+    other->element_bytes = 0;
+
+    return;
+  }
+
+  if (mvec->size + other->size > mvec->capacity) {
+    // vector is full
+
+    size_t new_capacity =
+        2 * ((other->size > mvec->capacity ? other->size : mvec->capacity));
+    size_t new_size = new_capacity * mvec->element_bytes;
+    void* new_buffer = mckits_malloc(new_size);
+
+    memcpy(new_buffer, mvec->buffer, mvec->size * mvec->element_bytes);
+    mckits_free(mvec->buffer);
+    mvec->buffer = new_buffer;
+    mvec->capacity = new_capacity;
+  }
+
+  void* element = (uint8_t*)mvec->buffer + mvec->size * mvec->element_bytes;
+  memcpy(element, other->buffer, other->size * other->element_bytes);
+  mvec->size += other->size;
+
+  other->buffer = 0;
+  other->size = 0;
+  other->capacity = 0;
+  other->element_bytes = 0;
+}
+
+void mckits_vec_clear(struct MckitsVector* mvec) { mvec->size = 0; }
+
+void* mckits_vec_as_ptr(struct MckitsVector* mvec) { return mvec->buffer; }
+
+void mckits_vec_remove(struct MckitsVector* mvec, size_t index) {
+  assert(index < mvec->size);
+
+  if (index + 1 < mvec->size) {
+    void* dst = (uint8_t*)mvec->buffer + index * mvec->element_bytes;
+    void* src = (uint8_t*)mvec->buffer + (index + 1) * mvec->element_bytes;
+    size_t n = mvec->size - index - 1;
+    memmove(dst, src, n);
+  }
+  mvec->size -= 1;
 }
--- mckits/mstl/mckits_vector.h
+++ mckits/mstl/mckits_vector.h
@@ -9,15 +9,80 @@ extern "C" {
 #endif
 
 struct MckitsVector;
+
+/*
+@brief: Create a new vector.
+@param element_bytes[in]: each element size in byte
+*/
 struct MckitsVector mckits_vec_new(size_t element_bytes);
+
+/*
+@brief: Create a new vector with capacity.
+@param element_bytes[in]: each element size in byte
+@param capacity[in]: the new vector's capacity.
+*/
 struct MckitsVector mckits_vec_with_capacity(size_t element_bytes,
                                              size_t capacity);
+
+/*
+@brief: Returns the number of elements in the vector.
+*/
 size_t mckits_vec_len(const struct MckitsVector* mvec);
+
+/*
+@brief: Returns the total number of elements the vector can hold without
+  reallocating.
+*/
 size_t mckits_vec_capacity(const struct MckitsVector* mvec);
+
+/*
+@brief: Check if the vector is empty.
+@return:
+  1: means vector is empty.
+  0: means vector is not empty.
+*/
 int mckits_vec_is_empty(const struct MckitsVector* mvec);
+
+/*
+@brief: Appends an element to the back of vector.
+*/
 void mckits_vec_push(struct MckitsVector* mvec, void* value);
-void* mckits_vec_pop(struct MckitsVector* mvec);
+
+/*
+@brief: Removes the last element from a vector.
+*/
+void mckits_vec_pop(struct MckitsVector* mvec);
+
+/*
+@brief: Get the mvec[index] element.
+@return the pointer to the element. Or NULL if index is valid.
+*/
 void* mckits_vec_get(struct MckitsVector* mvec, size_t index);
+
+/*
+@brief: Moves all the elements of other into mvec, leaving other empty.
+*/
+void mckits_vec_append(struct MckitsVector* mvec, struct MckitsVector* other);
+
+/*
+@brief: Clears the vector, removing all values.
+@notes: this method has no effect on the allocated capacity of the vector.
+*/
+void mckits_vec_clear(struct MckitsVector* mvec);
+
+/*
+@brief: Returns the pointer to the vector buffer.
+*/
+void* mckits_vec_as_ptr(struct MckitsVector* mvec);
+
+/*
+@brief: Removes the element at position index within the vector,
+  shifting all elements after it to the left.
+@notes: Because this shifts over the remaining elements, it has a worst-case
+  performance of O(n).
+@panics: Panics if index is out of bounds.
+*/
+void mckits_vec_remove(struct MckitsVector* mvec, size_t index);
 
 #ifdef __cplusplus
 }
