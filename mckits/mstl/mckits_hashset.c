358add7be798c01a4a6a78245cf4d74e84d3b9d0
HuangJian <1342042894@qq.com>
2024-01-23 17:42:32 +0800

    [mckits] update hashset

--- mckits/mstl/mckits_hashset.c
+++ mckits/mstl/mckits_hashset.c
@@ -1,1 +1,67 @@
 #include "mckits_hashset.h"
+
+#include "mckits_hashmap.h"
+#include "mckits_malloc.h"
+
+struct MckitsHashSet {
+  struct MckitsHashMap* map;
+};
+
+struct MckitsHashSet* mckits_hashset_new() {
+  struct MckitsHashSet* mset =
+      (struct MckitsHashSet*)mckits_malloc(sizeof(struct MckitsHashSet));
+  mset->map = mckits_hashmap_new();
+  return mset;
+}
+
+struct MckitsHashSet* mckits_hashset_with_capacity(size_t capacity) {
+  struct MckitsHashSet* mset =
+      (struct MckitsHashSet*)mckits_malloc(sizeof(struct MckitsHashSet));
+  mset->map = mckits_hashmap_with_capacity(capacity);
+  return mset;
+}
+
+void mckits_hashset_set_hashfunc(struct MckitsHashSet* mset,
+                                 uint64_t (*hash_func)(void* value,
+                                                       uint64_t seed0,
+                                                       uint64_t seed1)) {
+  mckits_hashmap_set_hashfunc(mset->map, hash_func);
+}
+
+void mckits_hashset_set_seed0(struct MckitsHashSet* mset, uint64_t seed0) {
+  mckits_hashmap_set_seed0(mset->map, seed0);
+}
+
+void mckits_hashset_set_seed1(struct MckitsHashSet* mset, uint64_t seed1) {
+  mckits_hashmap_set_seed1(mset->map, seed1);
+}
+
+void mckits_hashset_set_compare_func(struct MckitsHashSet* mset,
+                                     int (*compare_func)(void* a, void* b)) {
+  mckits_hashmap_set_compare_func(mset->map, compare_func);
+}
+
+void mckits_hashset_set_free_value(struct MckitsHashSet* mset,
+                                   void (*free_value)(void* value)) {
+  mckits_hashmap_set_free_key(mset->map, free_value);
+}
+
+void mckits_hashset_drop(struct MckitsHashSet* mset) {
+  mckits_hashmap_drop(mset->map);
+}
+
+void mckits_hashset_clear(struct MckitsHashSet* mset) {
+  mckits_hashmap_clear(mset->map);
+}
+
+size_t mckits_hashset_len(struct MckitsHashSet* mset) {
+  return mckits_hashmap_len(mset->map);
+}
+
+int mckits_hashset_is_empty(struct MckitsHashSet* mset) {
+  return mckits_hashmap_is_empty(mset->map);
+}
+
+void mckits_hashset_insert(struct MckitsHashSet* mset, void* value) {
+  mckits_hashmap_insert(mset->map, value, value);
+}
--- mckits/mstl/mckits_hashset.h
+++ mckits/mstl/mckits_hashset.h
@@ -8,6 +8,83 @@
 extern "C" {
 #endif
 
+/*
+@brief: hashset
+*/
+struct MckitsHashSet;
+
+/*
+@brief: Create a new hashset.
+*/
+struct MckitsHashSet* mckits_hashset_new();
+
+/*
+@brief: Create a new hashset with capacity.
+*/
+struct MckitsHashSet* mckits_hashset_with_capacity(size_t capacity);
+
+/*
+@brief: Set hash function, it will be used to calculated value hash.
+*/
+void mckits_hashset_set_hashfunc(struct MckitsHashSet* mset,
+                                 uint64_t (*hash_func)(void* value,
+                                                       uint64_t seed0,
+                                                       uint64_t seed1));
+
+/*
+@brief: Set seed0, used in hash function.
+*/
+void mckits_hashset_set_seed0(struct MckitsHashSet* mset, uint64_t seed0);
+
+/*
+@brief: Set seed1, used in hash function.
+*/
+void mckits_hashset_set_seed1(struct MckitsHashSet* mset, uint64_t seed1);
+
+/*
+@brief: Set compare function, it will be used to compare to two value.
+*/
+void mckits_hashset_set_compare_func(struct MckitsHashSet* mset,
+                                     int (*compare_func)(void* a, void* b));
+
+/*
+@brief: Set free value function. If your value need to free, you need to set
+this function. If your value doesn't need to free, you can set it to NULL.
+Default is NULL.
+*/
+void mckits_hashset_set_free_value(struct MckitsHashSet* mset,
+                                   void (*free_value)(void* value));
+
+/*
+@brief: Drop the hashset. After call this function, hashset object can't be
+  used again.
+*/
+void mckits_hashset_drop(struct MckitsHashSet* mset);
+
+/*
+@brief: Clear the hashset. Remove all values. Your can insert after call this
+  function.
+*/
+void mckits_hashset_clear(struct MckitsHashSet* mset);
+
+/*
+@brief: Returns the number of elements in the set.
+*/
+size_t mckits_hashset_len(struct MckitsHashSet* mset);
+
+/*
+@brief: Returns true if the set contains no elements.
+@return:
+  1: means hashset is empty.
+  0: means hashset is not empty.
+*/
+int mckits_hashset_is_empty(struct MckitsHashSet* mset);
+
+/*
+@brief: Insert new value into hashset.
+*/
+void mckits_hashset_insert(struct MckitsHashSet* mset, void* value);
+
 #ifdef __cplusplus
 }
 #endif
