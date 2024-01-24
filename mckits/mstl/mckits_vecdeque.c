f5066e68c1d0975b89185acb8a08739139995946
HuangJian <1342042894@qq.com>
2024-01-24 12:15:33 +0800

    [mckits] update VecDeque

--- mckits/mstl/mckits_vecdeque.c
+++ mckits/mstl/mckits_vecdeque.c
@@ -1,1 +1,70 @@
 #include "mckits_vecdeque.h"
+
+#include <string.h>
+
+#include "mckits_malloc.h"
+
+struct MckitsVecDeque* mckits_vecdeque_new(size_t element_bytes) {
+  struct MckitsVecDeque* vecdeque =
+      (struct MckitsVecDeque*)mckits_malloc(sizeof(struct MckitsVecDeque));
+  memset(vecdeque, 0, sizeof(struct MckitsVecDeque));
+  vecdeque->element_bytes = element_bytes;
+  return vecdeque;
+}
+
+struct MckitsVecDeque* mckits_vecdeque_with_capacity(size_t element_bytes,
+                                                     size_t capacity) {
+  struct MckitsVecDeque* vecdeque =
+      (struct MckitsVecDeque*)mckits_malloc(sizeof(struct MckitsVecDeque));
+  vecdeque->size = 0;
+  vecdeque->capacity = capacity;
+  vecdeque->element_bytes = element_bytes;
+  vecdeque->buffer = mckits_malloc(capacity * element_bytes);
+  vecdeque->head = 0;
+  return vecdeque;
+}
+
+void mckits_vecdeque_init(struct MckitsVecDeque* vecdeque,
+                          size_t element_bytes) {
+  memset(vecdeque, 0, sizeof(struct MckitsVecDeque));
+  vecdeque->element_bytes = element_bytes;
+}
+
+void mckits_vecdeque_init_with_capacity(struct MckitsVecDeque* vecdeque,
+                                        size_t element_bytes, size_t capacity) {
+  vecdeque->size = 0;
+  vecdeque->capacity = capacity;
+  vecdeque->element_bytes = element_bytes;
+  vecdeque->buffer = mckits_malloc(capacity * element_bytes);
+  vecdeque->head = 0;
+}
+
+void mckits_vecdeque_drop(struct MckitsVecDeque* vecdeque) {
+  mckits_vecdeque_drop_data(vecdeque);
+  mckits_free(vecdeque);
+}
+
+void mckits_vecdeque_drop_data(struct MckitsVecDeque* vecdeque) {
+  if (vecdeque->buffer == NULL) {
+    return;
+  }
+
+  mckits_free(vecdeque->buffer);
+  vecdeque->buffer = NULL;
+  vecdeque->size = 0;
+  vecdeque->capacity = 0;
+  vecdeque->element_bytes = 0;
+  vecdeque->head = 0;
+}
+
+size_t mckits_vecdeque_len(const struct MckitsVecDeque* vecdeque) {
+  return vecdeque->size;
+}
+
+size_t mckits_vecdeque_capacity(const struct MckitsVecDeque* vecdeque) {
+  return vecdeque->capacity;
+}
+
+int mckits_vecdeque_is_empty(const struct MckitsVecDeque* vecdeque) {
+  return vecdeque->size == 0 ? 1 : 0;
+}
--- mckits/mstl/mckits_vecdeque.h
+++ mckits/mstl/mckits_vecdeque.h
@@ -8,6 +8,77 @@
 extern "C" {
 #endif
 
+struct MckitsVecDeque {
+  // pointer to array
+  void* buffer;
+  // the number of elements in buffer. eg. size(), len()
+  size_t size;
+  // max element buffer can hold. eg. cap(), capacity()
+  size_t capacity;
+  // each element size in byte
+  size_t element_bytes;
+  // head of queue
+  size_t head;
+};
+
+/*
+@brief: Create a new VecDeque.
+@param element_bytes[in]: each element size in byte
+*/
+struct MckitsVecDeque* mckits_vecdeque_new(size_t element_bytes);
+
+/*
+@brief: Create a new VecDeque with capacity.
+@param element_bytes[in]: each element size in byte
+@param capacity[in]: the new VecDeque's capacity.
+*/
+struct MckitsVecDeque* mckits_vecdeque_with_capacity(size_t element_bytes,
+                                                     size_t capacity);
+
+/*
+@brief: Initialize VecDeque.
+@param element_bytes[in]: each element size in byte
+*/
+void mckits_vecdeque_init(struct MckitsVecDeque* vecdeque,
+                          size_t element_bytes);
+
+/*
+@brief: Initialize VecDeque.
+@param element_bytes[in]: each element size in byte
+@param capacity[in]: the new VecDeque's capacity.
+*/
+void mckits_vecdeque_init_with_capacity(struct MckitsVecDeque* vecdeque,
+                                        size_t element_bytes, size_t capacity);
+
+/*
+@brief: Drop the VecDeque.
+*/
+void mckits_vecdeque_drop(struct MckitsVecDeque* vecdeque);
+
+/*
+@brief: Drop the VecDeque's data field, reset the len/cap/head to zero.
+*/
+void mckits_vecdeque_drop_data(struct MckitsVecDeque* vecdeque);
+
+/*
+@brief: Returns the number of elements in the VecDeque.
+*/
+size_t mckits_vecdeque_len(const struct MckitsVecDeque* vecdeque);
+
+/*
+@brief: Returns the total number of elements the VecDeque can hold without
+  reallocating.
+*/
+size_t mckits_vecdeque_capacity(const struct MckitsVecDeque* vecdeque);
+
+/*
+@brief: Check if the VecDeque is empty.
+@return:
+  1: means VecDeque is empty.
+  0: means VecDeque is not empty.
+*/
+int mckits_vecdeque_is_empty(const struct MckitsVecDeque* vecdeque);
+
 #ifdef __cplusplus
 }
 #endif
