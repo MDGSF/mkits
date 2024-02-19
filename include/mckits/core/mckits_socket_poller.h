902b679be30e6db78e94805096ad0fb79197a382
HuangJian <1342042894@qq.com>
2024-02-19 15:18:25 +0800

    docs: add socket poller comments

--- include/mckits/core/mckits_socket_poller.h
+++ include/mckits/core/mckits_socket_poller.h
@@ -29,33 +29,55 @@ struct MckitsPollerEvent {
 bool mckits_socket_poller_is_valid(mckits_poller_fd fd);
 
 /*
-@brief:
+@brief: Create a new poller.
+@return: new poller file descriptor.
 */
 mckits_poller_fd mckits_socket_poller_new();
 
 /*
-@brief:
+@brief: Drop the poller.
 */
 void mckits_socket_poller_drop(mckits_poller_fd fd);
 
 /*
-@brief:
+@brief: Add a socket to poller.
+@param fd[in]: poller file descriptor.
+@param sock[in]: socket file descriptor.
+@param user_data[in]: user data bind to `sock` file descriptor.
+@return:
+  On success, 0 is returned.
+  On error, -1 is returned.
 */
 int mckits_socket_poller_add(mckits_poller_fd fd, int sock, void* user_data);
 
 /*
-@brief:
+@brief: Delete a socket from poller.
+@param fd[in]: poller file descriptor.
+@param sock[in]: socket file descriptor.
 */
 void mckits_socket_poller_del(mckits_poller_fd fd, int sock);
 
 /*
-@brief:
+@brief: Modify socket event.
+@param fd[in]: poller file descriptor.
+@param sock[in]: socket file descriptor.
+@param user_data[in]: user data bind to `sock` file descriptor.
+@param read_enable[in]: enable or disable read event.
+@param write_enable[in]: enable or disable write event.
+@return:
+  On success, 0 is returned.
+  On error, -1 is returned.
 */
 int mckits_socket_poller_enable(mckits_poller_fd fd, int sock, void* user_data,
                                 bool read_enable, bool write_enable);
 
 /*
-@brief:
+@brief: Wait events from poller.
+@param fd[in]: poller file descriptor.
+@param event[out]: Output events array.
+@param max_event[in]: `event` array size. Means wants max event from poller.
+@return: The number of events returned from poller.
+  Smaller or equal to `max_event`.
 */
 int mckits_poller_poller_wait(mckits_poller_fd fd,
                               struct MckitsPollerEvent* event, int max_event);
