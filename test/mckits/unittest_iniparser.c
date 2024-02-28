7977ed3c22fed8def3d6e9e874736eed22152249
HuangJian <1342042894@qq.com>
2024-02-28 14:08:46 +0800

    test: update iniparser unit test

--- test/mckits/unittest_iniparser.c
+++ test/mckits/unittest_iniparser.c
@@ -261,10 +261,38 @@ void test04() {
   mckits_iniparser_drop(iniparser);
 }
 
+void test05() {
+  struct MckitsIniParser* iniparser =
+      mckits_iniparser_new(MCKITS_INIPARSER_DEFAULT_DELIMITERS,
+                           MCKITS_INIPARSER_DEFAULT_COMMENT_PREFIXS);
+
+  const char* data =
+      "ip=127.0.0.1\n"
+      "port=12306\n"
+      "[section1]\n"
+      "name=John\n"
+      "age=18\n"
+      "\n"
+      "[section2]\n"
+      "book_name=harry potter\n"
+      "price=28\n";
+
+  int ret = mckits_iniparser_from_cstring(iniparser, data);
+  assert(ret == 0);
+
+  assert(1 == mckits_iniparser_has_entry(
+                  iniparser, MCKITS_INIPARSER_DEFAULT_SECTION, "ip"));
+  assert(1 == mckits_iniparser_has_entry(
+                  iniparser, MCKITS_INIPARSER_DEFAULT_SECTION, "port"));
+
+  mckits_iniparser_drop(iniparser);
+}
+
 int main() {
   test01();
   test02();
   test03();
   test04();
+  test05();
   return 0;
 }
