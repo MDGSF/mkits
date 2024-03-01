d9755fec7c8c7d4d7a2eb9db3cea1754347e6aaf
HuangJian <1342042894@qq.com>
2024-03-01 11:55:42 +0800

    feat: update calc 24

--- apps/Calc24point/Calc24Server/main.cpp
+++ apps/Calc24point/Calc24Server/main.cpp
@@ -7,21 +7,16 @@
 #include "TCPServer.h"
 #include "mckits/core/mckits_mlog.h"
 
-TCPServer* pserver = nullptr;
-
-void sig_handler(int sig) {
-  printf("sig = %d\n", sig);
-  pserver->stop();
-}
+void sig_handler(int sig) { printf("sig = %d\n", sig); }
 
 int main(int argc, char** argv) {
   signal(SIGPIPE, SIG_IGN);
-  signal(SIGINT, sig_handler);  // ctrl + C
-  signal(SIGHUP, sig_handler);  // close tty
-  signal(SIGQUIT, sig_handler);
-  signal(SIGTERM, sig_handler);
-  signal(SIGUSR1, sig_handler);
-  signal(SIGKILL, sig_handler);
+  // signal(SIGINT, sig_handler);  // ctrl + C
+  // signal(SIGHUP, sig_handler);  // close tty
+  // signal(SIGQUIT, sig_handler);
+  // signal(SIGTERM, sig_handler);
+  // signal(SIGUSR1, sig_handler);
+  // signal(SIGKILL, sig_handler);
   // signal(SIGSEGV, sigsegv_handler);
   // signal(SIGBUS, sigsegv_handler);
 
@@ -35,16 +30,14 @@ int main(int argc, char** argv) {
   const char* ip = argv[1];
   const char* port = argv[2];
 
-  pserver = new TCPServer();
-  int ret = pserver->init(ip, atoi(port));
+  TCPServer server;
+  int ret = server.init(ip, atoi(port));
   if (ret != 0) {
     errlog("tcp server init failed, ret = %d", ret);
     return 0;
   }
 
-  pserver->start();
-
-  delete pserver;
+  server.start();
 
   return 0;
 }
